92aa2a18c22f287c2d1694c51e3683f23cb4e45b
Ilnyr Nazargulov <ilnyr.nazargulov@ddgcorp.ru>
2021-03-02 12:59:03 +0300

    some change

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -26,7 +26,7 @@
         >
       </li>
     </ul>
-    <h3>Essential Links</h3>
+    <h3>БУБА</h3>
     <ul>
       <li>
         <a href="https://vuejs.org" target="_blank" rel="noopener">Core Docs</a>
