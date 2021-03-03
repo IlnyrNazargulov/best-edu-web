faab2894f33e6cdd18e49ed866b3b692a433a826
Ilnyr Nazargulov <ilnyr.nazargulov@ddgcorp.ru>
2021-03-03 19:13:21 +0300

    change app.vue

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,19 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+  <h1 @click="onClick">
+    {{ message }}
+  </h1>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import Vue from "vue";
+import Component from "vue-class-component";
 
-export default {
-  name: 'App',
-  components: {
-    HelloWorld
+export default Component({})(
+  class App extends Vue {
+    message = "Карина";
+    onClick() {
+      this.message = "жопка";
+    }
   }
-}
+);
 </script>
-
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
