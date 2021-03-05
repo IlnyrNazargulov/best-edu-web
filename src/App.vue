48d4c406bc4e28bf0aa7d79377d80239cadc5993
Ilnyr Nazargulov <ilnyr.nazargulov@ddgcorp.ru>
2021-03-05 20:47:24 +0300

    add simple request

--- src/App.vue
+++ src/App.vue
@@ -1,18 +1,36 @@
 <template>
-  <h1 @click="onClick">
-    {{ message }}
-  </h1>
+  <div>
+    <input required v-model="email" type="email" placeholder="Name" />
+    <p>{{ message }}</p>
+    <button type="button" @click="onClick">Some action</button>
+  </div>
 </template>
 
 <script>
 import Vue from "vue";
 import Component from "vue-class-component";
+import axios from "axios";
 
 export default Component({})(
   class App extends Vue {
-    message = "Карина";
+    email = "";
+    message = "";
     onClick() {
-      this.message = "жопка";
+      this.message = "Письмо отправлено на почтовый адрес: " + this.email;
+      let createRequestCode = {
+        email: this.email,
+      };
+      axios({
+        url: "https://best-edu.tk/api/v1/accounts/request-code/",
+        data: createRequestCode,
+        method: "POST",
+      })
+        .then((resp) => {
+          console.log(resp);
+        })
+        .catch((err) => {
+          console.log(err);
+        });
     }
   }
 );
