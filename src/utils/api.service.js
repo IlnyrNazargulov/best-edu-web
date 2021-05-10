5c966dad8731497ce6256bb838acdcfba8fc3200
IlnyrNazargulov <ilnnazargulov@gmail.com>
2021-05-10 21:16:30 +0300

    add interceptors

--- src/utils/api.service.js
+++ src/utils/api.service.js
@@ -8,6 +8,51 @@ export const ApiService = {
   init() {
     Vue.use(VueAxios, axios);
     Vue.axios.defaults.baseURL = API_URL;
+    Vue.axios.interceptors.response.use(
+      (response) => {
+        return response;
+      },
+      (error) => {
+        if (error.response.status !== 401) {
+          return new Promise((resolve, reject) => {
+            reject(error);
+          });
+        }
+        return new Promise((resolve, reject) => {
+          delete Vue.axios.defaults.headers.common["Authorization"];
+          Vue.axios
+            .post("accounts/current/refresh/", {
+              token: JwtService.getRefreshToken(),
+            })
+            .then((response) => {
+              JwtService.saveAccessToken(response.data.data.access_token);
+              JwtService.saveRefreshToken(response.data.data.refresh_token);
+              resolve(response.data.data.access_token);
+            })
+            .catch((error) => {
+              reject(error);
+            });
+        })
+          .then((token) => {
+            const config = error.config;
+            config.headers["Authorization"] = `Bearer ${token}`;
+
+            return new Promise((resolve, reject) => {
+              axios
+                .request(config)
+                .then((response) => {
+                  resolve(response);
+                })
+                .catch((error) => {
+                  reject(error);
+                });
+            });
+          })
+          .catch((error) => {
+            Promise.reject(error);
+          });
+      }
+    );
   },
   setAuthorizationHeader(token = null) {
     Vue.axios.defaults.headers.common["Authorization"] = `Bearer ${
